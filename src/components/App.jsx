b4eeb367044077abdfaa2c7ebea0993b79f98656
RomanBratus <romanbratus@gmail.com>
2023-03-24 10:01:10 +0200

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import Profile from './Profile';
-import user from 'components/user.json';
 import Statistics from './Statistics';
+
+
+import user from 'components/user.json';
 import data from 'components/data.json';
 
 export const App = () => {
@@ -15,13 +17,14 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+      {/*<Profile*/}
+      {/*  username={user.username}*/}
+      {/*  tag={user.tag}*/}
+      {/*  location={user.location}*/}
+      {/*  avatar={user.avatar}*/}
+      {/*  stats={user.stats}*/}
+      {/*/>*/}
+
       <Statistics title="Upload stats" stats={data.uploadStats} />
       <Statistics title="Views stats" stats={data.viewsStats} />
       <Statistics title="Financial stats" stats={data.financialStats} />
